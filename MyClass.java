0817ee6e3a1bec49c628bc2c1d8b63457e6fe0c2
alex-ncsr <alex.nujnii@gmail.com>
2022-07-17 17:18:37 +0300

    Add file MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass{
+	public static void main(String[] args){
+		System.out.println("This is my first application in alex-ncsr repo");
+		System.out.println("Hello World!");
+	}
+}
